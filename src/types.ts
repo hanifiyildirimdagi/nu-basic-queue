33221f077a3cf20fa1de146c3c13b462074b4383
Hanifi YILDIRIMDAGI <h.yildirimdagi@amatis.nl>
2022-06-10 15:17:26 +0300

    Custom Interceptor Type Created

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
 import { BasicQueueMessage } from "./BasicQueueMessage";
+import IBasicQueue from "./IBasicQueue";
 
 export type HandlerFunction<T extends BasicQueueMessage> = (
   message: T
 ) => Promise<void>;
+
+export type CustomizeInterceptorHandler<T extends BasicQueueMessage> = (
+  messageCount: number,
+  currentMessage: T | null
+) => Promise<boolean>;
