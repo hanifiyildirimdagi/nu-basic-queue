53589003ab418852fc2aa923e1560c018165fa89
Hanifi YILDIRIMDAGI <h.yildirimdagi@amatis.nl>
2022-06-15 08:12:22 +0300

    Local Utils Removed

--- src/BasicQueueMessage.ts
+++ src/BasicQueueMessage.ts
@@ -1,11 +1,11 @@
-import { uuidv4 } from "./utils";
+import {UUID} from "necessary-utils-core"
 
 export class BasicQueueMessage {
     public Id: string;
     public Date: number;
     public UnAck: boolean = false;
     constructor() {
-      this.Id = uuidv4();
+      this.Id = UUID.V4();
       this.Date = new Date().getTime();
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import BasicQueue from "./BasicQueue";
 import { BasicQueueMessage } from "./BasicQueueMessage";
 import QueueSettings from "./QueueSettings";
-import { wait, uuidv4 } from "./utils";
 
-export { BasicQueue, BasicQueueMessage, QueueSettings, wait, uuidv4 };
+export { BasicQueue, BasicQueueMessage, QueueSettings };
